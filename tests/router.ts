a91db0b5efcf45ef0887c4c9703f90fb8295df49
Dante Issaias <dante@issaias.com>
2022-05-18 22:31:55 +0100

    Fix test

--- tests/router.ts
+++ tests/router.ts
@@ -10,7 +10,7 @@ async function send(app: Application, pathname: string) {
 Deno.test("hello world", async () => {
   const app = new Application();
   const router = new Router();
-  router.all("/", () => Response.json({}));
+  router.all("/", () => "hello world");
   app.use(router.routes());
   const res = await send(app, "/");
   assertEquals(res.status, 200);
