b8a5182cd2ffd4c46ec13067f878abe791aa94c7
Dante Issaias <dante@issaias.com>
2022-06-14 12:31:24 +0100

    Add test

--- tests/app.ts
+++ tests/app.ts
@@ -1,2 +1,10 @@
-// TODO(danteissaias): Add new tests
-Deno.test("application", () => {});
+import { assertEquals } from "https://deno.land/std@0.143.0/testing/asserts.ts";
+import { Application } from "../application.ts";
+
+Deno.test("application", async () => {
+  const app = new Application();
+  app.get("/", () => new Response("Hello, World!"));
+  const req = new Request("http://localhost:8000/");
+  const res = await app.handle(req);
+  assertEquals(await res.text(), "Hello, World!");
+});
