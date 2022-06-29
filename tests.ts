c90872370076a3ad1156a8ba7608190c6c8101e2
Dante Issaias <dante@issaias.com>
2022-06-29 14:27:27 +0100

    New tests

--- tests.ts
+++ tests.ts
@@ -5,16 +5,28 @@ const app = new Application();
 
 app.get("/", () => "Hello, World!");
 
-Deno.test("200", async () => {
+Deno.test("app.handle", async () => {
   const req = new Request("http://localhost:8000");
   const res = await app.handle(req);
   assertEquals(res.status, 200);
   assertEquals(await res.text(), "Hello, World!");
+
+  const req2 = new Request("http://localhost:8000/404");
+  const res2 = await app.handle(req2);
+  assertEquals(res2.status, 404);
+  assertEquals(await res2.text(), "Not Found");
 });
 
-Deno.test("404", async () => {
-  const req = new Request("http://localhost:8000/404");
+app.use(async (ctx, next) => {
+  const res = await next(ctx);
+  res.headers.set("x-hello", "world");
+  return res;
+});
+
+Deno.test("app.use", async () => {
+  const req = new Request("http://localhost:8000");
   const res = await app.handle(req);
-  assertEquals(res.status, 404);
-  assertEquals(await res.text(), "Not Found");
+  assertEquals(res.status, 200);
+  assertEquals(await res.text(), "Hello, World!");
+  assertEquals(res.headers.get("x-hello"), "world");
 });
