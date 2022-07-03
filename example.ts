d25a4deb4a21f76b7093484dc3cb6bed0d245ead
Dante Issaias <dante@issaias.com>
2022-07-03 02:32:00 +0100

    Fix tests

--- example.ts
+++
@@ -1,8 +0,0 @@
-import { serve } from "https://deno.land/std@0.145.0/http/server.ts";
-import { Application } from "https://deno.land/x/fast@0.2.0/mod.ts";
-
-const app = new Application();
-
-app.get("/", () => "Hello, World!");
-
-serve(app.handle);
--- tests.ts
+++ tests.ts
@@ -3,7 +3,14 @@ import { Application } from "./mod.ts";
 
 const app = new Application();
 
+app.use(async (ctx, next) => {
+  const res = await next(ctx);
+  res.headers.set("x-hello", "world");
+  return res;
+});
+
 app.get("/", () => "Hello, World!");
+app.get("/json", () => ({ hello: "world" }));
 
 Deno.test("app.handle", async () => {
   const req = new Request("http://localhost:8000");
@@ -15,12 +22,11 @@ Deno.test("app.handle", async () => {
   const res2 = await app.handle(req2);
   assertEquals(res2.status, 404);
   assertEquals(await res2.text(), "Not Found");
-});
 
-app.use(async (ctx, next) => {
-  const res = await next(ctx);
-  res.headers.set("x-hello", "world");
-  return res;
+  const req3 = new Request("http://localhost:8000/json");
+  const res3 = await app.handle(req3);
+  assertEquals(res3.status, 200);
+  assertEquals(await res3.json(), { hello: "world" });
 });
 
 Deno.test("app.use", async () => {
