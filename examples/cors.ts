33ad5b43960ee765a88f6dd2ab694d2cc2835b98
Dante Issaias <dante@issaias.com>
2022-07-05 15:13:30 +0100

    Fix cors

--- examples/cors.ts
+++ examples/cors.ts
@@ -1,9 +1,7 @@
 import type { Middleware } from "../mod.ts";
 
 export const cors: Middleware = async (ctx, next) => {
-  const { method } = ctx.request;
-  const isPreflight = method === "OPTIONS";
-  if (isPreflight) {
+  if (ctx.request.method === "OPTIONS") {
     const headers = new Headers();
     headers.set("Allow", "*");
     headers.set("Access-Control-Allow-Origin", "*");
@@ -11,7 +9,7 @@ export const cors: Middleware = async (ctx, next) => {
     headers.set("Access-Control-Allow-Headers", "*");
     return new Response(null, { status: 204, headers });
   } else {
-    const res = await next(ctx);
+    const res = await next();
     res.headers.set("Access-Control-Allow-Origin", "*");
     res.headers.set("Access-Control-Allow-Methods", "*");
     res.headers.set("Access-Control-Allow-Headers", "*");
