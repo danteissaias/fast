1a495f0e17a8bd980b42434f1290060c5c8cba7f
Dante Issaias <dante@issaias.com>
2022-07-20 23:34:56 +0100

    Fix exports

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
 import { Application } from "./server/application.ts";
 export default () => new Application();
 export { Application } from "./server/application.ts";
-export { Context, ServerError } from "./server/context.ts";
+export { Context } from "./server/context.ts";
 export { compose } from "./server/middleware.ts";
 export type { Middleware, NextFunction } from "./server/middleware.ts";
