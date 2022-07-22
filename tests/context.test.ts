c86bea3ec32bd411f3a057d361ad1dd54ed1b484
Dante Issaias <dante@issaias.com>
2022-07-22 20:04:37 +0100

    Fix tests

--- tests/context.test.ts
+++ tests/context.test.ts
@@ -4,7 +4,7 @@ import {
 } from "https://deno.land/std@0.149.0/testing/asserts.ts";
 import { Context } from "../mod.ts";
 
-const request = new Request("http://localhost:8000");
+const request = new Request("http://localhost:8000/?id=1");
 const params = {};
 const ctx = new Context({ request, params });
 
@@ -22,4 +22,15 @@ Deno.test("ctx.redirect", () => {
   const res = ctx.redirect("/hello");
   const to = res.headers.get("location");
   assertEquals(to, "http://localhost:8000/hello");
+});
+
+Deno.test("ctx.redirect", () => {
+  const res = ctx.redirect("/hello");
+  const to = res.headers.get("location");
+  assertEquals(to, "http://localhost:8000/hello");
+});
+
+Deno.test("ctx.params", () => {
+  const id = ctx.query["id"];
+  assertEquals(id, "1");
 });
