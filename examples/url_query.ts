93019915728952f6725931043f0d25eed7fceffd
omar2205 <omar2205a@yahoo.com>
2022-07-22 22:24:40 +0200

    New example (#7)

---
+++ examples/url_query.ts
@@ -0,0 +1,12 @@
+import fast from "../mod.ts";
+
+const app = fast();
+
+app.get("/", (ctx) => {
+  const name = ctx.query["name"];
+  if (name) return `Hello, ${name}`;
+
+  return "Hello, world";
+});
+
+await app.serve();
