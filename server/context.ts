2c8d7c8bbc5f74c90d5bd2f4d05eaf6cb0a31fd4
omar2205 <omar2205a@yahoo.com>
2022-07-22 17:35:03 +0000

    add ctx.query

--- server/context.ts
+++ server/context.ts
@@ -26,11 +26,13 @@ export class Context {
   params: Record<string, string>;
   state: State;
   #url: URL | undefined;
+  query: Record<string, string>;
 
   constructor({ request, params }: ContextInit) {
     this.request = request;
     this.params = params ?? {};
     this.state = new State();
+    this.query = Object.fromEntries((this.url).searchParams.entries())
   }
 
   get url() {
